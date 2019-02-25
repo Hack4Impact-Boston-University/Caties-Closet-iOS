5c375a2541aa3754805e4961059a802c5e26c053
mhendrick98 <mhendric@bu.edu>
2019-02-25 14:42:25 -0500

    added template code for app

---
+++ Caties-Closet-iOS/Caties-Closet-iOS/ViewControllerAboutUs.swift
@@ -0,0 +1,30 @@
+//
+//  ViewControllerAboutUs.swift
+//  Caties-Closet-iOS
+//
+//  Created by Michael Hendrick on 2/25/19.
+//  Copyright © 2019 Hack4ImpactBostonUniversity. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerAboutUs: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Caties-Closet-iOS/Caties-Closet-iOS/ViewControllerLogin.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Caties-Closet-iOS
+//
+//  Created by Michael Hendrick on 2/25/19.
+//  Copyright © 2019 Hack4ImpactBostonUniversity. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerLogin: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+
+
+}
+
