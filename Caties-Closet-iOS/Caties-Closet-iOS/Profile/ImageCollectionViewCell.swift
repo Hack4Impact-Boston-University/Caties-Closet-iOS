5ad6ef2bdc478b826bd134d5dc956f5eedd1d4eb
Juanzgc <juanzgc@bu.edu>
2019-03-07 22:48:29 -0500

    Added the first implementation of the profile page.

---
+++ Caties-Closet-iOS/Caties-Closet-iOS/Profile/ImageCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ImageCollectionViewCell.swift
+//  Caties-Closet-iOS
+//
+//  Created by Juan Zapata on 3/7/19.
+//  Copyright © 2019 Hack4ImpactBostonUniversity. All rights reserved.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+
+
+    @IBOutlet weak var img: UIImageView!
+    @IBOutlet weak var imgLabel: UILabel!
+    
+}
